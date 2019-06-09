8b335285888c11b8a7619f8fb4a8d0cdb2876bda
kishore.donthikurthi <kishore.donthikurthi@ispace.com>
2019-06-09 18:53:40 +0530

    songs upload #1

--- notes.md
+++ notes.md
@@ -1,21 +1,34 @@
 ### Commands and Comments
 1. Build automation using make
-2. Add CNAME
+2. To use a custom domain with GitHub Pages, you need to put the domain of your site (e.g., blog.example.com) inside a CNAME file at the root of your site. To do this, create the content/extra/ directory and add a CNAME file to it. Then use the STATIC_PATHS setting to tell Pelican to copy this file to your output directory. For example:  
+    ~~~python   
+    STATIC_PATHS = ['images', 'extra/CNAME']
+    EXTRA_PATH_METADATA = {'extra/CNAME': {'path': 'CNAME'},}
+    ~~~
+
 3. Add google analytics
-4. themes 
-    - elegant, Flex, html5-dopetrope, pelican-blue, pelican-striped-html5up
+4. themes to tryout
+    - elegant, Flex, html5-dopetrope, pelican-blue, pelican-striped-html5up, fresh
     - 
 5. Plugins
     - English & Telugu versions
         http://smartass101.github.io/pelican-plugins/
         http://smartass101.github.io/pelican-plugins/category/i18n_subsites.html
 
-6. Search functionality
+6. Search functionality - google custom search vs tipue 
 
 7. Template for the article
 
-8. CNAME preserve while pushing
+8. DEFAULT_LANG
 
 9. choosing custom domain
 
 10. source in master & content in gh-pages
+
+11. add themes and plugins as submodules 
+
+12. Decide on genres(tags) - inspirational, devotional, classic, trending, actor/actresses.. 
+
+13. Viewers comments, requests - staticman vs Disqus
+
+14. Separate authors
--- pelicanconf.py
+++ pelicanconf.py
@@ -4,7 +4,9 @@ from __future__ import unicode_literals
 
 AUTHOR = 'maatemantram'
 SITENAME = 'మాటేమంత్రం'
-SITEURL = 'https://maatemantram.com'
+# SITEURL = 'https://maatemantram.com'
+SITEURL = 'http://localhost:8000'
+
 
 PATH = 'content'
 
@@ -34,16 +36,37 @@ DEFAULT_PAGINATION = False
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
 
-PLUGIN_PATHS = ["pelican-plugins"]
-PLUGINS = ["tag_cloud"]
+
+STATIC_PATHS = ['images', 'extra/CNAME']
+EXTRA_PATH_METADATA = {'extra/CNAME': {'path': 'CNAME'},}
+
+
+''' We have the option to define where Pelican should look for our blog's pages. 
+By default Pelican expects them to be in the content/pages folder. 
+It is not necessary to state the path but it is a good practice to do so. '''
+# PAGE_PATHS = ['pages']
+
+'''To change the URL to show the content type and date as well. 
+The ARTICLE_URL variable states what should display in the web browser's address bar 
+while the ARTICLE_SAVE_AS variable defines where the article being generated should be output to.
+ARTICLE_URL = 'articles/{date:%Y}/{date:%m}/{date:%d}/{slug}/' '''
+USE_FOLDER_AS_CATEGORY = True
+
+# ARTICLE_PATHS = ['articles',]
+ARTICLE_URL = '{category}/{slug}'
+ARTICLE_SAVE_AS = ARTICLE_URL+'.html'
 
 
 THEME='themes/pelican-fresh'
 # Theme specific configuration
 
-GOOGLE_CUSTOM_SEARCH = 'your search engine unique ID'
+PLUGIN_PATHS = ["pelican-plugins"]
+PLUGINS = ["tag_cloud"]
 
-HIDE_CATEGORIES_FROM_MENU = False
+
+# GOOGLE_CUSTOM_SEARCH = 'your search engine unique ID'
+
+HIDE_CATEGORIES_FROM_MENU = True
 
 # SHARETHIS_PUB_KEY = 'your ShareThis Pub Key'
 
